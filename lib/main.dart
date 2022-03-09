809a2de0e1424f644dbf812889be717af3519381
Devallina <34373731+easton06@users.noreply.github.com>
2022-03-10 00:41:45 +0800

    monthly calendar with a selector

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:math' as math;
 
 void main() => runApp(const MyApp());
 
@@ -12,6 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: appTitle,
       home: MyHomePage(title: appTitle),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -24,23 +27,17 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: GridView.count(
-        crossAxisCount: 7,
-        children: List.generate(31, (index) {
-          return Container(
-            decoration: BoxDecoration(
-              color: index % 2 == 1 ? Colors.deepOrange : Colors.black26,
-            ),
-            child: Center(
-              child: Text(
-                'Day $index',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-            ),
-          );
-        }),
-      ),
+      appBar: AppBar(title: Text(title), actions: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.add_alert),
+          tooltip: 'Show Snackbar',
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('This is a snackbar')));
+          },
+        ),
+      ]),
+      body: const MonthSelection(),
       drawer: Drawer(
         // Add a ListView to the drawer. This ensures the user can scroll
         // through the options in the drawer if there isn't enough vertical
@@ -74,6 +71,360 @@ class MyHomePage extends StatelessWidget {
               },
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+@immutable
+class MonthSelection extends StatefulWidget {
+  const MonthSelection({Key? key}) : super(key: key);
+
+  @override
+  _MonthSelectionState createState() =>
+      _MonthSelectionState();
+}
+
+class _MonthSelectionState
+    extends State<MonthSelection> {
+  final _months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  final thirtyOne = ["January", "March", "May", "July", "August", "October", "December"];
+
+  final _yearMonth = ValueNotifier<String>("January");
+
+  void _onMonthChanged(String value) {
+    _yearMonth.value = value;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.black,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: _buildCalendarWithMonth(),
+          ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: _buildMonthSelector(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCalendarWithMonth() {
+    return ValueListenableBuilder(
+      valueListenable: _yearMonth,
+      builder: (context, value, child) {
+        final text = value as String;
+        return Scaffold(
+          appBar: AppBar(title: Text(text)),
+          body: GridView.count(
+              crossAxisCount: 7,
+              children: [
+                ...List.generate(thirtyOne.contains(text)? 31 : text == "February"? 28 :30, (index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: index.isEven ? Colors.deepOrange : Colors.blue,
+                    ),
+                    child: Center(
+                      child: Text(
+                        '${index + 1}',
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ),
+                  );
+                }),
+              ]
+            ),
+        );
+      },
+    );
+  }
+
+  Widget _buildMonthSelector() {
+    return MonthSelector(
+      onMonthChanged: _onMonthChanged,
+      months: _months,
+    );
+  }
+}
+
+@immutable
+class MonthSelector extends StatefulWidget {
+  const MonthSelector({
+    Key? key,
+    required this.months,
+    required this.onMonthChanged,
+    this.padding = const EdgeInsets.symmetric(vertical: 24.0),
+  }) : super(key: key);
+
+  final List<String> months;
+  final void Function(String selectedMonth) onMonthChanged;
+  final EdgeInsets padding;
+
+  @override
+  _MonthSelectorState createState() => _MonthSelectorState();
+}
+
+class _MonthSelectorState extends State<MonthSelector> {
+  static const _monthsPerScreen = 5;
+  static const _viewportFractionPerItem = 1.0 / _monthsPerScreen;
+
+  late final PageController _controller;
+  late int _page;
+
+  int get monthCount => widget.months.length;
+
+  String itemMonth(int index) => widget.months[index % monthCount];
+
+  @override
+  void initState() {
+    super.initState();
+    _page = 0;
+    _controller = PageController(
+      initialPage: _page,
+      viewportFraction: _viewportFractionPerItem,
+    );
+    _controller.addListener(_onPageChanged);
+  }
+
+  void _onPageChanged() {
+    final page = (_controller.page ?? 0).round();
+    if (page != _page) {
+      _page = page;
+      widget.onMonthChanged(widget.months[page]);
+    }
+  }
+
+  void _onMonthTapped(int index) {
+    _controller.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 450),
+      curve: Curves.ease,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scrollable(
+      controller: _controller,
+      axisDirection: AxisDirection.right,
+      physics: const PageScrollPhysics(),
+      viewportBuilder: (context, viewportOffset) {
+        return LayoutBuilder(
+          builder: (context, constraints) {
+            final itemSize = constraints.maxWidth * _viewportFractionPerItem;
+            viewportOffset
+              ..applyViewportDimension(constraints.maxWidth)
+              ..applyContentDimensions(0.0, itemSize * (monthCount - 1));
+
+            return Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                _buildShadowGradient(itemSize),
+                _buildCarousel(
+                  viewportOffset: viewportOffset,
+                  itemSize: itemSize,
+                ),
+                _buildSelectionRing(itemSize),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildShadowGradient(double itemSize) {
+    return SizedBox(
+      height: itemSize * 2 + widget.padding.vertical,
+      child: const DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.transparent,
+              Colors.blueGrey,
+            ],
+          ),
+        ),
+        child: SizedBox.expand(),
+      ),
+    );
+  }
+
+  Widget _buildCarousel({
+    required ViewportOffset viewportOffset,
+    required double itemSize,
+  }) {
+    return Container(
+      height: itemSize,
+      margin: widget.padding,
+      child: Flow(
+        delegate: CarouselFlowDelegate(
+          viewportOffset: viewportOffset,
+          monthsPerScreen: _monthsPerScreen,
+        ),
+        children: [
+          for (int i = 0; i < monthCount; i++)
+            MonthItem(
+              onMonthSelected: () => _onMonthTapped(i),
+              text: itemMonth(i),
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSelectionRing(double itemSize) {
+    return IgnorePointer(
+      child: Padding(
+        padding: widget.padding,
+        child: SizedBox(
+          width: itemSize,
+          height: itemSize,
+          child: const DecoratedBox(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.fromBorderSide(
+                BorderSide(width: 6.0, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
+class CarouselFlowDelegate extends FlowDelegate {
+  CarouselFlowDelegate({
+    required this.viewportOffset,
+    required this.monthsPerScreen,
+  }) : super(repaint: viewportOffset);
+
+  final ViewportOffset viewportOffset;
+  final int monthsPerScreen;
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    final count = context.childCount;
+
+    // All available painting width
+    final size = context.size.width;
+
+    // The distance that a single item "page" takes up from the perspective
+    // of the scroll paging system. We also use this size for the width and
+    // height of a single item.
+    final itemExtent = size / monthsPerScreen;
+
+    // The current scroll position expressed as an item fraction, e.g., 0.0,
+    // or 1.0, or 1.3, or 2.9, etc. A value of 1.3 indicates that item at
+    // index 1 is active, and the user has scrolled 30% towards the item at
+    // index 2.
+    final active = viewportOffset.pixels / itemExtent;
+
+    // Index of the first item we need to paint at this moment.
+    // At most, we paint 3 items to the left of the active item.
+    final min = math.max(0, active.floor() - 3).toInt();
+
+    // Index of the last item we need to paint at this moment.
+    // At most, we paint 3 items to the right of the active item.
+    final max = math.min(count - 1, active.ceil() + 3).toInt();
+
+    // Generate transforms for the visible items and sort by distance.
+    for (var index = min; index <= max; index++) {
+      final itemXFromCenter = itemExtent * index - viewportOffset.pixels;
+      final percentFromCenter = 1.0 - (itemXFromCenter / (size / 2)).abs();
+      final itemScale = 0.5 + (percentFromCenter * 0.5);
+      final opacity = 0.25 + (percentFromCenter * 0.75);
+
+      final itemTransform = Matrix4.identity()
+        ..translate((size - itemExtent) / 2)
+        ..translate(itemXFromCenter)
+        ..translate(itemExtent / 2, itemExtent / 2)
+        ..multiply(Matrix4.diagonal3Values(itemScale, itemScale, 1.0))
+        ..translate(-itemExtent / 2, -itemExtent / 2);
+
+      context.paintChild(
+        index,
+        transform: itemTransform,
+        opacity: opacity,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CarouselFlowDelegate oldDelegate) {
+    return oldDelegate.viewportOffset != viewportOffset;
+  }
+}
+
+@immutable
+class MonthItem extends StatelessWidget {
+  const MonthItem({
+    Key? key,
+    required this.text,
+    this.onMonthSelected,
+  }) : super(key: key);
+
+  final String text;
+  final VoidCallback? onMonthSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onMonthSelected,
+      child: AspectRatio(
+        aspectRatio: 1.0,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ClipOval(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.lightBlue,
+                border: Border.all(
+                  color: Colors.blueGrey,
+                  width: 1.0,
+                ),
+                shape: BoxShape.circle,
+              ),
+              child: Center(
+                child: Text(
+                  text,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            )
+          ),
         ),
       ),
     );
